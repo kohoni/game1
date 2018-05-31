086c61d0427bd7aff576b49d709328c1b7515e10
Kohei Onishi <onishifundal@gmail.com>
2018-05-31 22:56:06 +0900

    Add files via upload

---
+++ script/Move5.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move5 : MonoBehaviour {
+    public float speed;
+	Rigidbody rb;
+	public float teraspeed;
+	public bool IsPlaying = true;
+	//public bool isPlaying = true;
+
+	// Use this for initialization
+	void Start () {
+  		rb = gameObject.GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (IsPlaying == true) {
+			if (Input.GetKey (KeyCode.Z)) {
+				Vector3 pos = transform.position;
+				pos.x = -30;
+				transform.position = pos;
+			}
+			if (Input.GetKey (KeyCode.X)) {
+				Vector3 pos = transform.position;
+				pos.x = 0;
+				transform.position = pos;
+			}
+			if (Input.GetKey (KeyCode.C)) {
+				Vector3 pos = transform.position;
+				pos.x = 30;
+				transform.position = pos;
+			}
+			//automatic movement
+			teraspeed = teraspeed + (speed * Time.deltaTime);
+			transform.position += new Vector3 (0.0f, 0.0f, teraspeed);
+		}
+	}
+
+	// ぶつかった瞬間に呼び出される
+	void OnCollisionEnter(Collision c)
+	{
+		// レイヤー名を取得
+		string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+		// レイヤー名がBullet (Enemy)の時は弾を削除
+		if( layerName == "Object")
+		{
+			IsPlaying = false;
+			Debug.Log("hit!");
+			teraspeed = 0;
+		}
+			
+	}
+}
---
+++ script/scoreupdater.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class scoreupdater : MonoBehaviour {
+
+	public GameObject Player;
+	public Text Score;
+	private int score = 0;
+	private int high = 0;
+	private string hig = "high";
+	public bool IsPlaying = true;
+	public GameObject gameover;
+
+	void Start()
+	{
+		gameover.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{   
+		if (IsPlaying == true) {
+
+			int score = (int)Player.transform.localPosition.z;
+			Score.text = score + "km";
+
+			if (PlayerPrefs.GetInt("highsaaaaaa") < score) {
+				PlayerPrefs.SetInt("highsaaaaaa",score);
+			}
+			//Save();
+		}
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		// レイヤー名を取得
+		string layerName = LayerMask.LayerToName(c.gameObject.layer);
+
+		// レイヤー名がBullet (Enemy)の時は弾を削除
+		if( layerName == "Object")
+		{
+			IsPlaying = false;
+			Gameover ();
+		}
+
+	}
+
+	/*int CalMeter()
+	{
+		return (int)Player.transform.position.z;
+	}*/
+
+	void Gameover()
+	{
+		gameover.SetActive (true);
+
+		/*if (high < score) 
+		{
+			high = score;
+			Save();
+		}*/
+
+		GetComponent<AudioSource> ().Play();
+
+
+		Debug.Log(PlayerPrefs.GetInt("highs"));
+
+		Invoke ("Gotoresult", 2.0f);
+	}
+
+	// ハイスコアの保存
+	/*public void Save ()
+	{
+		// ハイスコアを保存する
+		PlayerPrefs.SetInt ("hig", high);
+
+		// ゲーム開始前の状態に戻す
+		//Initialize ();
+	}*/
+
+	void Gotoresult()
+	{
+		SceneManager.LoadScene ("title");
+	}
+}
